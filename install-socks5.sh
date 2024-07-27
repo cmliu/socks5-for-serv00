732929045673c6234a757dee22d93f2eec9df17a
cmliu <admin@cmliu.net>
2024-07-28 03:39:46 +0800

    test

--- install-socks5.sh
+++ install-socks5.sh
@@ -267,7 +267,7 @@ echo "保存当前pm2进程列表"
 pm2 save
 
 echo "定义要添加的cron任务"
-CRON_JOB="*/12 * * * * /home/$(whoami)/.npm-global/lib/node_modules/pm2/bin/pm2 resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
+CRON_JOB="*/5 * * * * /home/$(whoami)/.npm-global/lib/node_modules/pm2/bin/pm2 resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 
 echo "检查crontab是否已存在该任务"
 (crontab -l | grep -F "$CRON_JOB") || (crontab -l; echo "$CRON_JOB") | crontab -
