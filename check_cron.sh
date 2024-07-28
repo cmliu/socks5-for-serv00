67f9d85895633c7545ac77364fd295739ecf7a79
cmliu <admin@cmliu.net>
2024-07-28 21:31:59 +0800

    test

--- check_cron.sh
+++ check_cron.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "定义要添加的cron任务"
 CRON_JOB="*/12 * * * * /home/$(whoami)/.npm-global/lib/node_modules/pm2/bin/pm2 resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 COMMAND_PATH="/home/$(whoami)/.npm-global/lib/node_modules/pm2/bin/pm2 resurrect"
+REBOOT_COMMAND="@reboot pkill -kill -u $(whoami) && /home/$(whoami)/.npm-global/lib/node_modules/pm2/bin/pm2 resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 
-echo "检查 crontab 是否已存在该任务"
+echo "检查并添加 crontab 重启任务"
+(crontab -l | grep -F "$REBOOT_COMMAND") || (crontab -l; echo "$REBOOT_COMMAND") | crontab -
+
+echo "检查并添加 crontab 保活任务"
 (crontab -l | grep -F "$COMMAND_PATH") || (crontab -l; echo "$CRON_JOB") | crontab -
