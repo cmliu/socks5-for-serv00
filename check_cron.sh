ee89c2bec02ebc707bcdb86c40617bb9ac6b687b
lmghl <laomghl@outlook.com>
2024-12-15 12:53:08 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -9,6 +9,10 @@ PM2_PATH="/home/${USER}/.npm-global/lib/node_modules/pm2/bin/pm2"
 CRON_JOB="*/12 * * * * $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 REBOOT_COMMAND="@reboot pkill -kill -u $(whoami) && $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 
+RESTART_GOST_PATH="/home/${USER}/restart_gost.sh"
+# 设置每天凌晨 3 点重启 gost
+CRON_RESTART_GOST="0 3 * * * bash ${RESTART_GOST_PATH} >/dev/null 2>&1"
+
 echo "检查并添加 crontab 任务"
 
 if [ "$(command -v pm2)" == "/home/${USER}/.npm-global/bin/pm2" ]; then
@@ -17,6 +21,18 @@ if [ "$(command -v pm2)" == "/home/${USER}/.npm-global/bin/pm2" ]; then
   (crontab -l | grep -F "$CRON_JOB") || (crontab -l; echo "$CRON_JOB") | crontab -
 else
   if [ -e "${WORKDIR}/start.sh" ] && [ -e "${FILE_PATH}/config.json" ]; then
+    if [ -e "$RESTART_GOST_PATH" ]; then
+      echo "发现 restart_gost.sh 文件，添加执行权限"
+      chmod +x $RESTART_GOST_PATH
+      echo "执行权限已添加，文件状态："
+      ls -l $RESTART_GOST_PATH  # 显示文件权限状态
+
+      echo "添加 restart_gost.sh 的定时任务"
+      (crontab -l | grep -F "$CRON_RESTART_GOST") || (crontab -l; echo "$CRON_RESTART_GOST") | crontab -
+      echo "已成功添加 restart_gost.sh 的定时任务"
+    else
+      echo "未发现 restart_gost.sh 文件，无需添加定时任务"
+    fi
     echo "添加 nezha & socks5 的 crontab 重启任务"
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
