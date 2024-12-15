c610b925fb9ac73968bbe653a2a252bd9ab96eca
lmghl <laomghl@outlook.com>
2024-12-15 12:50:15 +0800

    Update check_cron.sh

--- check_cron.sh
+++ check_cron.sh
@@ -9,10 +9,6 @@ PM2_PATH="/home/${USER}/.npm-global/lib/node_modules/pm2/bin/pm2"
 CRON_JOB="*/12 * * * * $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 REBOOT_COMMAND="@reboot pkill -kill -u $(whoami) && $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
 
-RESTART_GOST_PATH="/home/${USER}/restart_gost.sh"
-# 设置每天凌晨 3 点重启 gost
-CRON_RESTART_GOST="0 3 * * * bash ${RESTART_GOST_PATH} >/dev/null 2>&1"
-
 echo "检查并添加 crontab 任务"
 
 if [ "$(command -v pm2)" == "/home/${USER}/.npm-global/bin/pm2" ]; then
@@ -34,16 +30,4 @@ else
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
   fi
-if [ -e "$RESTART_GOST_PATH" ]; then
-  echo "发现 restart_gost.sh 文件，添加执行权限"
-  chmod +x $RESTART_GOST_PATH
-  echo "执行权限已添加，文件状态："
-  ls -l $RESTART_GOST_PATH  # 显示文件权限状态
-
-  echo "添加 restart_gost.sh 的定时任务"
-  (crontab -l | grep -F "$CRON_RESTART_GOST") || (crontab -l; echo "$CRON_RESTART_GOST") | crontab -
-  echo "已成功添加 restart_gost.sh 的定时任务"
-else
-  echo "未发现 restart_gost.sh 文件，无需添加定时任务"
-fi
 fi
